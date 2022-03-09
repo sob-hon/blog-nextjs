6e95245e896cf957d2f586f6611d5cc39f93bb5f
Sobhan <sobhanyazdanjo@gmail.com>
2022-03-09 08:02:17 +0330

    Fixed bug.

--- components/contact/ContactForm.js
+++ components/contact/ContactForm.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import classes from "./ContactForm.module.css";
-import Notification from "../ui/notification";
+import Notification from "../ui/Notification";
 
 const ContactForm = () => {
   const emailRef = useRef();
