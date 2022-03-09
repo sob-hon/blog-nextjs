c91c1c804299f59167dde6cc755f5a51928e7060
Sobhan <sobhanyazdanjo@gmail.com>
2022-03-09 08:56:28 +0330

    Add to packages to solve the deployment.

--- components/ui/Notification.js
+++ components/ui/Notification.js
@@ -1,22 +1,22 @@
-// import classes from "./Notification.module.css";
+import classes from "./Notification.module.css";
 
 function Notification(props) {
   const { title, message, status } = props;
 
-  // let statusClasses = "";
+  let statusClasses = "";
 
-  // if (status === "success") {
-  //   statusClasses = classes.success;
-  // }
+  if (status === "success") {
+    statusClasses = classes.success;
+  }
 
-  // if (status === "error") {
-  //   statusClasses = classes.error;
-  // }
+  if (status === "error") {
+    statusClasses = classes.error;
+  }
 
-  // const cssClasses = `${classes.notification} ${statusClasses}`;
+  const cssClasses = `${classes.notification} ${statusClasses}`;
 
   return (
-    <div>
+    <div className={cssClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
     </div>
